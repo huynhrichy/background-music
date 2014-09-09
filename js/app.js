f744def892d7fb20a409f4bb83887ea83f10fdd1
Richard Huynh <richardbaohuynh@gmail.com>
2014-09-09 16:47:41 -0400

    Adds autocomplete for cities and some styling

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 $(document).ready(function() {
+	jQuery("#city-input").autocomplete({
+		source: function (request, response) {
+			jQuery.getJSON(
+			"http://gd.geobytes.com/AutoCompleteCity?callback=?&q=" + request.term,
+			function (data) {
+				response(data);
+			}
+			);
+		},
+		minLength: 3,
+		select: function (event, ui) {
+			var selectedObj = ui.item;
+			jQuery("#city-input").val(selectedObj.value);
+			return false;
+		},
+		open: function () {
+			jQuery(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+		},
+		close: function () {
+			jQuery(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+		}
+	 });
+	 jQuery("#city-input").autocomplete("option", "delay", 100);
 });
