1a1bbf477f4c8c3cc0b02ff4f7d5b13516e4116b
Richard Huynh <richardbaohuynh@gmail.com>
2014-09-11 16:29:07 -0400

    Reloads on new city and implements browser geolocation

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 var song = "";
 var city = "";
-var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+var weatherURL = "http://api.openweathermap.org/data/2.5/weather?";
 var latitude;
 var longitude;
+var weatherType = "";
 
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 //firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+//getLocation();
+
+function getLocation() {
+	//alert();
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setCoordinates);
+    } 
+}
+function setCoordinates(position) {
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
+
+	getWeather();
+}
 
 var player;
 function onYouTubeIframeAPIReady() {
@@ -46,37 +64,123 @@ function showPlayer() {
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
-function decideSong() {
-	song = "ARDDZsIj5bE"; // besaid x-2
+function decideSong(icon) {
+	//alert(icon);
+
+		//if icon is clear sky
+		if(icon == '01d' || icon == '01n') {
+		
+			//iconName = 'clear';
+    		
+			//song = "ARDDZsIj5bE"; // besaid
+			song = "xwuXLqqw_gU"; // breezy
+		}
+		
+		//if icon is clouds
+		if(icon == '02d' || icon == '02n' || icon == '03d' || icon == '03n' || icon == '04d' || icon == '04n') {
+		
+			//iconName = 'clouds';
+			song = "utt7YbfkJvg"; // beyond the darkness
+    		
+		}
+		
+		//if icon is rain
+		if(icon == '09d' || icon == '09n' || icon == '10d' || icon == '10n') {
+		
+			//iconName = 'rain';
+    		
+    		song = "-15xE5U9BCQ"; // calm before the storm
+		}
+		
+		//if icon is thunderstorm
+		if(icon == '11d' || icon == '11n') {
+		
+			//iconName = 'storm';
+    		song = "7Ax45TprB4A";
+		}
+		
+		//if icon is snow
+		if(icon == '13d' || icon == '13n') {
+		
+			//iconName = 'snow';
+			//song = "9jz644GeAdE"; // macalania woods
+			song = "UgsC3fk7E-k" // phantoms
+    		
+		}
+		
+		//if icon is mist
+		if(icon == '50d' || icon == '50n') {
+		
+			//iconName = 'mist';
+			song = "i_QxGtXSa9E"; // secret of the forest
+		}
+
+
+
 }
 
 function getWeather() {
 	var request;
+	
+
+	weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude;
 
 	var result = $.ajax({
 		type: 'GET',
 		dataType: 'jsonp',
-		url: ''
+		url: weatherURL
 	})
 	.done(function(data) {
+		alert(data.weather[0].description);
+		//alert(data.weather[0].icon);
+
+		// decide song
+		
+			decideSong(data.weather[0].icon);
+
+			if (!($('iframe').length)) {
+
+				showPlayer();	
+			}
+			else {
+				// reload
+				player.loadVideoById(song);
+			}
+		        		
 
 	})
 	.fail(function(jqXHR, error, errorThrown) {
 	});
 }
 
+function getCityData() {
+	$.getJSON("http://gd.geobytes.com/GetCityDetails?callback=?&fqcn=" + city, 
+		function(data) {
+			latitude = data.geobyteslatitude;
+			longitude = data.geobyteslongitude;
+		})
+	.done(function() {
+		//alert(latitude + ' ' + longitude);
+		getWeather();
+	});
+}
+
 $(document).ready(function() {
+
+//getLocation();
+	
 	/* 
 	Geobyte's Ajax Autocomplete List Cities JSON-P API 
 	Accesses Geobyte's free web services to get the list of cities
 	*/
+
+	var ish;
 	$("#city-input").autocomplete({
 		source: function(request, response) {
 			$.getJSON(
 			"http://gd.geobytes.com/AutoCompleteCity?callback=?&q=" + request.term,
 			function(data) {
 				response(data);
-				//alert(data.geobyteslatitude);
 			}
 			);
 		},
@@ -85,12 +189,13 @@ $(document).ready(function() {
 			var selectedObj = ui.item;
 			$("#city-input").val(selectedObj.value);
 
+			//alert(ish.geobyteslatitude);
+
 			// weather ish
 			city = selectedObj.value;
 
-			decideSong();
+			getCityData();
 
-			showPlayer();
 			//
 
 			return false;
@@ -106,6 +211,3 @@ $(document).ready(function() {
 
 
 });
-
-
-
