e7f0a1b62d46ccc638f328c292fd72f788eaf521
Richard Huynh <richardbaohuynh@gmail.com>
2014-09-12 15:04:35 -0400

    Uncomments browser geolocation; adds info about APIs to footer

--- js/app.js
+++ js/app.js
@@ -1,26 +1,24 @@
-var song = "";
-var city = "";
-var weatherURL = "http://api.openweathermap.org/data/2.5/weather?";
-var latitude;
-var longitude;
+var song = ""; // YouTube video ID
+var latitude; 
+var longitude; 
 var weatherType = "";
 
-var tag = document.createElement('script');
+// Endpoint for the OpenWeatherMap API - string to be appended with location info
+var weatherURL = "http://api.openweathermap.org/data/2.5/weather?";
 
+// YouTube Player API
+// Creates an iframe for the DOM to embed the player
+var player;
+var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
-//firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-//getLocation();
 
 function getLocation() {
-	//alert();
-
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(setCoordinates);
     } 
 }
+
 function setCoordinates(position) {
 	latitude = position.coords.latitude;
 	longitude = position.coords.longitude;
@@ -28,30 +26,19 @@ function setCoordinates(position) {
 	getWeather();
 }
 
-var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
     	height: '196.875 ',
         width: '350',
-        //videoId: 'M7lc1UVf-VE',
        	videoId: song,
         events: {
         	'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
         }
     });
 }
 
 function onPlayerReady(event) {
     event.target.playVideo();
-}
-
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-		//setTimeout(stopVideo, 6000);
-		//done = true;
-    }
 }
 
 function stopVideo() {
@@ -64,65 +51,45 @@ function showPlayer() {
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
+// Song choice based on icons for weather types for simplicity
 function decideSong(icon) {
-	//alert(icon);
-
-		//if icon is clear sky
-		if(icon == '01d' || icon == '01n') {
+	//if icon is clear sky
+	if(icon == '01d' || icon == '01n') {
+		//song = "ARDDZsIj5bE"; // besaid
+		song = "xwuXLqqw_gU"; // breezy
+	}
+	
+	//if icon is clouds
+	if(icon == '02d' || icon == '02n' || icon == '03d' || icon == '03n' || icon == '04d' || icon == '04n') {
+		song = "utt7YbfkJvg"; // beyond the darkness
 		
-			//iconName = 'clear';
-    		
-			//song = "ARDDZsIj5bE"; // besaid
-			song = "xwuXLqqw_gU"; // breezy
-		}
+	}
+	
+	//if icon is rain
+	if(icon == '09d' || icon == '09n' || icon == '10d' || icon == '10n') {
+		song = "-15xE5U9BCQ"; // calm before the storm
+	}
+	
+	//if icon is thunderstorm
+	if(icon == '11d' || icon == '11n') {
+		song = "7Ax45TprB4A";
+	}
+	
+	//if icon is snow
+	if(icon == '13d' || icon == '13n') {
+		//song = "9jz644GeAdE"; // macalania woods
+		song = "UgsC3fk7E-k" // phantoms
 		
-		//if icon is clouds
-		if(icon == '02d' || icon == '02n' || icon == '03d' || icon == '03n' || icon == '04d' || icon == '04n') {
-		
-			//iconName = 'clouds';
-			song = "utt7YbfkJvg"; // beyond the darkness
-    		
-		}
-		
-		//if icon is rain
-		if(icon == '09d' || icon == '09n' || icon == '10d' || icon == '10n') {
-		
-			//iconName = 'rain';
-    		
-    		song = "-15xE5U9BCQ"; // calm before the storm
-		}
-		
-		//if icon is thunderstorm
-		if(icon == '11d' || icon == '11n') {
-		
-			//iconName = 'storm';
-    		song = "7Ax45TprB4A";
-		}
-		
-		//if icon is snow
-		if(icon == '13d' || icon == '13n') {
-		
-			//iconName = 'snow';
-			//song = "9jz644GeAdE"; // macalania woods
-			song = "UgsC3fk7E-k" // phantoms
-    		
-		}
-		
-		//if icon is mist
-		if(icon == '50d' || icon == '50n') {
-		
-			//iconName = 'mist';
-			song = "i_QxGtXSa9E"; // secret of the forest
-		}
-
-
-
+	}
+	
+	//if icon is mist
+	if(icon == '50d' || icon == '50n') {
+		song = "i_QxGtXSa9E"; // secret of the forest
+	}
 }
 
+// Uses OpenWeatherMap API to get weather information for the given coordinates
 function getWeather() {
-	var request;
-	
-
 	weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude;
 
 	var result = $.ajax({
@@ -131,50 +98,39 @@ function getWeather() {
 		url: weatherURL
 	})
 	.done(function(data) {
-		alert(data.weather[0].description);
-		//alert(data.weather[0].icon);
-
-		// decide song
-		
 			decideSong(data.weather[0].icon);
 
 			if (!($('iframe').length)) {
-
 				showPlayer();	
 			}
 			else {
-				// reload
 				player.loadVideoById(song);
 			}
-		        		
-
 	})
 	.fail(function(jqXHR, error, errorThrown) {
 	});
 }
 
-function getCityData() {
+// Uses Geobytes' free web services to get information about a city
+function getCityData(city) {
 	$.getJSON("http://gd.geobytes.com/GetCityDetails?callback=?&fqcn=" + city, 
 		function(data) {
 			latitude = data.geobyteslatitude;
 			longitude = data.geobyteslongitude;
 		})
 	.done(function() {
-		//alert(latitude + ' ' + longitude);
 		getWeather();
+	})
+	.fail(function(jqXHR, error, errorThrown) {
 	});
 }
 
 $(document).ready(function() {
-
-getLocation();
+	// Attempts to use HTML5 Geolocation first
+	getLocation();
 	
-	/* 
-	Geobyte's Ajax Autocomplete List Cities JSON-P API 
-	Accesses Geobyte's free web services to get the list of cities
-	*/
-
-	var ish;
+	// Geobyte's Ajax Autocomplete List Cities JSON-P API 
+	// Accesses Geobyte's free web services to get the list of cities
 	$("#city-input").autocomplete({
 		source: function(request, response) {
 			$.getJSON(
@@ -185,18 +141,12 @@ getLocation();
 			);
 		},
 		minLength: 3,
-		select: function(event, ui) {
-			var selectedObj = ui.item;
-			$("#city-input").val(selectedObj.value);
+		select: function(event, ui) {;
+			var city = ui.item.value;
 
-			//alert(ish.geobyteslatitude);
+			$("#city-input").val(city);
 
-			// weather ish
-			city = selectedObj.value;
-
-			getCityData();
-
-			//
+			getCityData(city);
 
 			return false;
 		},
